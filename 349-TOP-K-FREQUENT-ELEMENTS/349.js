9d3698254f39799995952ae9c0fdf576404b66e6
Abhoy Das <abhoy_das@epam.com>
2025-01-21 10:15:54 +0530

    _

--- 349-TOP-K-FREQUENT-ELEMENTS/349.js
+++ 349-TOP-K-FREQUENT-ELEMENTS/349.js
@@ -1,26 +1,54 @@
-Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+// Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+ 
+// Example 1:
+
+// Input: nums = [1,1,1,2,2,3], k = 2
+// Output: [1,2]
+
+// Example 2:
+
+// Input: nums = [1], k = 1
+// Output: [1]
 
  
 
-Example 1:
+// Constraints:
 
-Input: nums = [1,1,1,2,2,3], k = 2
-Output: [1,2]
-
-Example 2:
-
-Input: nums = [1], k = 1
-Output: [1]
+//     1 <= nums.length <= 105
+//     -104 <= nums[i] <= 104
+//     k is in the range [1, the number of unique elements in the array].
+//     It is guaranteed that the answer is unique.
 
  
 
-Constraints:
+// Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
 
-    1 <= nums.length <= 105
-    -104 <= nums[i] <= 104
-    k is in the range [1, the number of unique elements in the array].
-    It is guaranteed that the answer is unique.
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function (nums, k) {
+    let map = new Map()
+    for (let i = 0; i < nums.length; i++) {
+        if (!map.has(nums[i])) {
+            map.set(nums[i], 1)
+        } else {
+            map.set(nums[i], map.get(nums[i]) + 1)
+        }
+    }
+    let mapSort1 = new Map([...map.entries()].sort((a, b) => b[1] - a[1]))
 
- 
+    let result = []
+    let count = 0;
 
-Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+    for (let[key, value] of mapSort1.entries()) {
+        if (count < k) {
+            result.push(key)
+            count++;
+        }
+    }
+    return result
+
+}
